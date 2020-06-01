eed07b55798bb691bdbc75d0a9e8b61b35442d52
juanvan <arnone.john@gmail.com>
2020-06-01 14:47:49 -0400

    Add project files.

---
+++ ViewChangerAsync/Bootstrapper.cs
@@ -0,0 +1,62 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using ViewChangerAsync.ViewModels;
+
+namespace ViewChangerAsync
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+
+            //_container.Instance(ConfigureAutomapper());
+
+            //_container.Instance(_container)
+            //    .PerRequest<IProductEndpoint, ProductEndpoint>()
+            //    .PerRequest<ISalesEndpoint, SalesEndpoint>()
+            //    .PerRequest<IUserEndpoint, UserEndpoint>();
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            //.Singleton<ILoggedInUserModel, LoggedInUserModel>()
+            //.Singleton<IConfigHelper, ConfigHelper>();
+            //_container.CreateChildContainer();
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ ViewChangerAsync/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace ViewChangerAsync.ViewModels
+{
+    public class NavigationViewModel : Screen
+    {
+        protected override void OnViewReady(object view)
+        {
+            base.OnViewReady(view);
+            Caliburn.Micro.Action.Invoke(this, "OnViewReady", (DependencyObject)view);
+        }
+        public virtual IEnumerable<IResult> OnViewReady()
+        {
+            yield break;
+        }
+    }
+}
